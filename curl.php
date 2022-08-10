9538ef80ecb3a3b0a0368236d759e399bcf3bde1
何必是个憨憨 <109293445+hanhanheleilei@users.noreply.github.com>
2022-08-10 13:18:55 +0800

    Delete curl.php

--- curl.php
+++
@@ -1,3 +0,0 @@
- header("Content-type:application/octet-strem");//第一步通过http协议告诉向客户端其后要发送给你的数据是一个应用程序数据
- header('Content-disposition:attachment;filename=video.mp4');//第二步通过http协议告诉客户端其后发送给你的数据要作为一个附件保存
- echo flie_get_contents('video.mp4');//将数据发送给服务器
