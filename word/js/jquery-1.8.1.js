b3d57d08638b1c4d0edc43f7220ef3a465aaffad
何必是个憨憨 <109293445+hanhanheleilei@users.noreply.github.com>
2022-09-11 18:53:24 +0800

    Delete jquery-1.8.1.js

--- word/js/jquery-1.8.1.js
+++
@@ -1,9 +0,0 @@
-animate({
-
-                            left: 0
-
-                        }, 8000, 'linear', function({
-
-                            $(this).remove();
-
-                        });
